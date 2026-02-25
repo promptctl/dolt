c9dfbc59335f9f097fc928d7e043eaf031d64f1d
Zach Musgrave <zach@dolthub.com>
2026-02-25 12:14:01 -0800

    gutted maptest

--- go/store/types/map.go
+++ go/store/types/map.go
@@ -299,32 +299,6 @@ func (m Map) isPrimitive() bool {
 
 type mapIterAllCallback func(key, value Value) error
 
-func (m Map) IterAll(ctx context.Context, cb mapIterAllCallback) error {
-	var k Value
-	err := iterAll(ctx, m, func(v Value, _ uint64) error {
-		if k != nil {
-			err := cb(k, v)
-
-			if err != nil {
-				return err
-			}
-
-			k = nil
-		} else {
-			k = v
-		}
-
-		return nil
-	})
-
-	if err != nil {
-		return err
-	}
-
-	d.PanicIfFalse(k == nil)
-	return nil
-}
-
 func (m Map) IterRange(ctx context.Context, startIdx, endIdx uint64, cb mapIterAllCallback) error {
 	var k Value
 	_, err := iterRange(ctx, m, startIdx, endIdx, func(v Value) error {
